82f692e091c0238cc16bc4c8a314bc8bd4a3edb8
Priscilla Salazar <pris@Priscillas-MacBook-Air.local>
2024-03-02 18:47:16 +0100

    add plots and prints

--- section1.py
+++ section1.py
@@ -1,9 +1,9 @@
 import numpy as np
 class domain: 
-    def __init__(self):
-        self.current_state = (3,0)
+    def __init__(self,w=0):
+        self.current_state = (0,0)
         self.m,self.n=5,5
-        self.w=np.random.uniform(0,1)
+        self.w=w
                                         
     def get_current_state(self):
         return self.current_state
@@ -23,7 +23,6 @@ class domain:
         return (current_state,action,new_state,self.reward(new_state,action))
 
     def dynamic(self,state, action):
-        print(self.w)
         if self.w<=0.5:
             return (min(max(state[0]+action[0],0),self.n-1),min(max(state[1]+action[1],0),self.m-1))
         else:
@@ -38,8 +37,33 @@ class agent:
         policy= (0,1)#self.action_space[np.random.randint(0,4)]
         return policy
 
-d = domain()
-a = agent()
-for i in range(10):
-    current_action = a.chose_action(d.get_current_state())
-    #print(d.step(current_action))
+sthocastic=True
+episodes=1
+steps=10
+initial_state=(3,0)
+domaintype=['deterministic', 'non-deterministic']
+
+# if sthocastic:
+#     episodes=10
+# savereward=np.zeros((episodes))
+# etoprint=np.random.randint(0,episodes)#pick a random episode to print
+# print("Initial state: ",initial_state,'Domain: ',domaintype[sthocastic])
+# print('Printing episode',etoprint+1,'of',episodes)
+# for e in range (episodes):
+#     d = domain()
+#     a = agent()
+#     d.current_state=initial_state
+#     for i in range(steps):
+#         if sthocastic:
+#             d.w=np.random.uniform(0,1)
+#         current_action = a.chose_action(d.get_current_state())
+#         step=d.step(current_action)
+#         if e== etoprint: #Print only one episode
+#             print('From state', step[0],'take action', step[1],
+#             ' New state:', step[2], 'with reward',step[3])
+#         savereward[e]+=step[3]
+
+# mean=np.average(savereward)
+# sd=np.sqrt(np.var(savereward))
+# print('Average total reward for',episodes,'episodes','of',steps,'steps each:',
+#       format(mean, '.2f'),'SD',format(sd, '.2f'))
--- section2.py
+++ section2.py
@@ -1,31 +1,70 @@
 import numpy as np
-from section1 import domain
-from section1 import agent
+from section1 import domain,agent
+import matplotlib.pyplot as plt
 
 class ereturn:
     def __init__(self):
-        self.gamma = 0.95
-        self.d=domain()
-    def function_j(self, state, agent, N):
-        self.d.w=np.random.uniform(0,1)
-        self.d.current_state=state
-        action=agent.chose_action(self.d.get_current_state())
-        self.d.step(action)
-        r=self.d.reward(self.d.get_current_state(),action)
+        self.gamma = 0.99
+
+    def function_j(self, state, agent, N,sthocastic):
+        if sthocastic:
+            dist=np.random.uniform(0,1)
+        else:
+            dist=0
+        self.d=domain(w=dist)
+        action=agent.chose_action(state)
+        new_state=self.d.dynamic(state,action)
+        r=self.d.reward(new_state,action)
         if N==0:
             return 0
         else:
-            rew=r+self.gamma*self.function_j(self.d.get_current_state(),agent,N-1)
-            return rew
-a = agent()
-init_state=(3,0)
-n=100
-print('for N=',n, 'return is ', ereturn().function_j(init_state, a, n))
+            #print(N,new_state,action,r)
+            rew=r+self.gamma*self.function_j(new_state,agent,N-1,sthocastic)    
+        return rew
 
-#max_reward=19
-#cum_return=[]
-#episodes=np.arange(10,10000,10)
-# for n in episodes: 
-#     bound=ereturn().gamma**n*max_reward/(1-ereturn().gamma)
-#     cum_return.append(ereturn().function_j(init_state, a, n))
-#     print('for N=',n, 'return is ', ereturn().function_j(init_state, a, n))
+#Find steps
+gamma = ereturn().gamma
+reward_bound=19
+epsilon,max_N=9e-2,1e10
+j_diff_bound=[]
+for N in range(int(max_N)):
+    j_diff_bound.append((reward_bound/(1-gamma))*gamma**N)
+    if j_diff_bound[-1] <=epsilon:
+        break
+
+#plt.figure()
+#plt.plot(j_diff_bound)
+#plt.xlabel('N [-]')
+#plt.ylabel(r"$\frac{\gamma^N}{1-\gamma}B_r$ [-]")
+#plt.grid()
+#plt.show()
+
+steps=10#N
+initial_state=(3,0)
+sthocastic=True
+domaintype=['deterministic', 'non-deterministic']
+if sthocastic:
+    episodes=20000
+else:
+    episodes=1
+save_j=np.zeros((episodes))
+
+state_space=[(x, y) for x in range(domain().m) for y in range(domain().n)]
+save_j_s_av=np.zeros((domain().m,domain().n))
+save_j_s_sd=np.zeros((domain().m,domain().n))
+for initial_state in state_space:
+    #print("Initial state: ",initial_state,'Domain: ',domaintype[sthocastic])
+    for e in range (episodes):
+         a = agent()
+         er=ereturn()
+         cum_return=er.function_j(initial_state, a, steps,sthocastic)
+         save_j[e]=(cum_return)
+         #print('for N=',steps, 'return is ', cum_return )
+    mean=np.average(save_j)
+    sd=np.sqrt(np.var(save_j))
+    #print('Average total reward for',episodes,'episodes','of',steps,'steps each:',
+    #   format(mean, '.2f'),'SD',format(sd, '.2f'))
+    save_j_s_av[initial_state[0],initial_state[1]]=format(mean, '.2f')
+    save_j_s_sd[initial_state[0],initial_state[1]]=format(sd, '.2f')
+print(save_j_s_av)
+print(save_j_s_sd)
