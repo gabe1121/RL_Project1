bdb9e198c810d20535089ff225e5eacc7be582e4
Priscilla Salazar <pris@Priscillas-MacBook-Air.local>
2024-03-03 01:02:47 +0100

    add option to print results

--- section1.py
+++ section1.py
@@ -37,33 +37,35 @@ class agent:
         policy= (0,1)#self.action_space[np.random.randint(0,4)]
         return policy
 
-sthocastic=True
-episodes=1
-steps=10
-initial_state=(3,0)
-domaintype=['deterministic', 'non-deterministic']
+compute=False
+if compute:
+    stochastic=True
+    episodes=1
+    steps=10
+    initial_state=(3,0)
+    domaintype=['deterministic', 'non-deterministic']
 
-# if sthocastic:
-#     episodes=10
-# savereward=np.zeros((episodes))
-# etoprint=np.random.randint(0,episodes)#pick a random episode to print
-# print("Initial state: ",initial_state,'Domain: ',domaintype[sthocastic])
-# print('Printing episode',etoprint+1,'of',episodes)
-# for e in range (episodes):
-#     d = domain()
-#     a = agent()
-#     d.current_state=initial_state
-#     for i in range(steps):
-#         if sthocastic:
-#             d.w=np.random.uniform(0,1)
-#         current_action = a.chose_action(d.get_current_state())
-#         step=d.step(current_action)
-#         if e== etoprint: #Print only one episode
-#             print('From state', step[0],'take action', step[1],
-#             ' New state:', step[2], 'with reward',step[3])
-#         savereward[e]+=step[3]
+    if stochastic:
+        episodes=10
+    savereward=np.zeros((episodes))
+    etoprint=np.random.randint(0,episodes)#pick a random episode to print
+    print("Initial state: ",initial_state,'Domain: ',domaintype[stochastic])
+    print('Printing episode',etoprint+1,'of',episodes)
+    for e in range (episodes):
+        d = domain()
+        a = agent()
+        d.current_state=initial_state
+        for i in range(steps):
+            if stochastic:
+                d.w=np.random.uniform(0,1)
+            current_action = a.chose_action(d.get_current_state())
+            step=d.step(current_action)
+            if e== etoprint: #Print only one episode
+                print('From state', step[0],'take action', step[1],
+                ' New state:', step[2], 'with reward',step[3])
+            savereward[e]+=step[3]
 
-# mean=np.average(savereward)
-# sd=np.sqrt(np.var(savereward))
-# print('Average total reward for',episodes,'episodes','of',steps,'steps each:',
-#       format(mean, '.2f'),'SD',format(sd, '.2f'))
+    mean=np.average(savereward)
+    sd=np.sqrt(np.var(savereward))
+    print('Average total reward for',episodes,'episodes','of',steps,'steps each:',
+        format(mean, '.2f'),'SD',format(sd, '.2f'))
