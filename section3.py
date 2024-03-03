dc8727091795adae3d960d479eedb46ec094edbd
LPris <prisci.salazar@gmail.com>
2024-03-03 17:25:58 +0100

    print optimal policy

--- section3.py
+++ section3.py
@@ -48,17 +48,43 @@ def function_q(domain, agent, state, action, N, stocha=False):
 
         return compute_mean_reward(domain, state, action, stocha) + domain.gamma * sum(item_to_sum)
 
+class mdpagent:
+    def __init__(self,state_space,action_space,Qfunction):
+        self.q=Qfunction
+        self.states=state_space
+        self.actions=action_space
+    def chose_action(self,state):
+        s=self.states.index(state)
+        policy= self.actions[np.argmax(self.q[s])]
+        return policy
+    
+d = Domain()
+a = Agent()
 
-# d = Domain()
-# a = Agent()
+state_space=[(x, y) for x in range(d.m) for y in range(d.n)] #[state_ini]
+action_space=a.action
+save_q=np.zeros((4,5,5))
+save_policy=np.empty((5,5),dtype=object)
+q_a_=np.zeros((len(state_space),len(action_space)))
 # state = [0, 4]
+for state in state_space:
 # # print(compute_mean_reward(d, state, [0, 1]))
-#
-# for n in range(10):
-#     # print(function_q(d, a, state, [0, 1], n))
-#     q_a = []
-#     for action in a.action:
-#         q_a.append(function_q(d, a, state, action, n, True))
-#
-#     print(q_a)
-#     print(np.argmax(q_a))
+    s=state_space.index(state)
+    for j in range (len(a.action)):
+        for n in [9]:#range(10):
+#           # print(function_q(d, a, state, [0, 1], n))
+            q_a = []
+            q_a_[s][j]=function_q(d, a, state, a.action[j], n, True)
+            #q_a.append(q_a_[state][j])
+        save_q[j,state[0],state[1]]=format(q_a_[s][j], '.2f')
+print(save_q)
+#******************************************************************************
+#Optimal policy
+#******************************************************************************
+a1 = mdpagent(state_space,action_space,q_a_)
+for state in state_space:
+    s=state_space.index(state)
+    save_policy[state[0],state[1]]=a1.chose_action(state)
+
+print('Optimal policy')
+print(save_policy)
